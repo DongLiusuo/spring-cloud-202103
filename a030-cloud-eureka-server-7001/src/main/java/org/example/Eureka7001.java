d5321ee017a0574499be6738a5c1ba828c3c0076
DongLiusuo <dongliusuo@qq.com>
2021-03-07 16:47:03 +0800

    eureka服务注册中心的搭建

---
+++ a030-cloud-eureka-server-7001/src/main/java/org/example/Eureka7001.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class Eureka7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka7001.class, args);
+    }
+}
