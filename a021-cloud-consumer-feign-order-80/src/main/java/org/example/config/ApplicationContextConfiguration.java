5bd11eaf4e2d8d70cb33fb7a81b6457f296a8076
DongLiusuo <dongliusuo@qq.com>
2021-03-08 21:34:15 +0800

    openFeign配置日志打印

--- a021-cloud-consumer-feign-order-80/src/main/java/org/example/config/ApplicationContextConfiguration.java
+++ a021-cloud-consumer-feign-order-80/src/main/java/org/example/config/ApplicationContextConfiguration.java
@@ -1,7 +1,9 @@
 package org.example.config;
 
+import feign.Logger;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,4 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableFeignClients(basePackages = "org.example.service") // 配置OpenFeign
 public class ApplicationContextConfiguration {
 
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
