930c6c31a219b43eee4ba6ba2954f614e1ae56d3
DongLiusuo <dongliusuo@qq.com>
2021-03-09 22:27:51 +0800

    使用 Hystrix Dashboard 监控a012

---
+++ a050-cloud-hystrix-dashboard-9001/src/main/java/org/example/HystrixDashboard9001.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard // 开启hystrix dashboard
+public class HystrixDashboard9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9001.class, args);
+    }
+}
