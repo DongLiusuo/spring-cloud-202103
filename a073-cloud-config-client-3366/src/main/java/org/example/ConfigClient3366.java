ee21b5453dea9342ba35886e482ad099730f68b0
DongLiusuo <dongliusuo@qq.com>
2021-03-14 19:36:04 +0800

    spring cloud bus消息总线

---
+++ a073-cloud-config-client-3366/src/main/java/org/example/ConfigClient3366.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClient3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3366.class, args);
+    }
+}
